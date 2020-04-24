c3b43a84a524b64989f130e11542f9d9acb43064
kevin431 <focus431@hotmail.com>
2020-04-24 23:38:11 +0800

    修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,9 +17,30 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="{{route('login')}}">登录</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ route('register')}}">注册</a></li>
+        @guest
+            <li class="nav-item"><a class="nav-link" href="{{route('login')}}">登录</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('register')}}">注册</a></li>
+        @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="20px" height="20px">
+                    {{Auth::user()->name}}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="">個人帳戶</a>
+                    <a class="dropdown-item" href="">編輯資料</a>
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" id="logout" href="#">
+                            <form action="{{ route('logout') }}" method="POST">
+                               {{ csrf_field() }}
+                               <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                            </form>
+                        </a>
+                    
+                </div>
+            </li>  
+        @endguest   
       </ul>
     </div>
-  </div>
+</div>
 </nav>
