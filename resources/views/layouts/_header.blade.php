5c39264255c7b770a09cd18648aeae03d5b911f2
kevin431 <focus431@hotmail.com>
2020-04-28 12:05:49 +0800

    显示头像

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -23,7 +23,7 @@
         @else
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="20px" height="20px">
+                    <img src="{{$user->avatar}}" class="img-responsive img-circle" width="20px" height="20px">
                     {{Auth::user()->name}}
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
